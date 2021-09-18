1bb083346f89795602b22642823b7a4857e867fc
DylanMey <dylan@synctivate.com>
2021-09-18 18:36:25 -0400

    All tests are now work

--- gps.py
+++ gps.py
@@ -6,6 +6,7 @@ Dylan Meyers, University of Mary Washington, fall 2021
 '''
 
 from math import inf
+from os import listxattr
 from atlas import Atlas
 import numpy as np
 import logging
@@ -24,53 +25,83 @@ def find_best_path(atlas):
     num_cities = atlas.get_num_cities()
     nodes = {}
     
-    number_to_add = 0
+    length = 0
     i = 0
+    list_added = {}
     while(i != num_cities-1):
-        # print(i)
+        # length = length + 1
+        # if length > 10:
+        #      break
+        
         dict_1 = {}
+    
         for x in range(num_cities):
+            next_item = {}
+            if atlas.get_road_dist(i,x) != inf and i != x and "{}".format(x) not in nodes :
+                if "{}".format(i) not in nodes:
+                    nodes["{}".format(i)] = {"{}".format(x): []}
+                if "{}".format(x) not in nodes["{}".format(i)]:
+                    nodes["{}".format(i)]["{}".format(x)] = []
 
-            if atlas.get_road_dist(i,x) != inf and i != x:
-                dict_1["{}".format(x)] = number_to_add + atlas.get_road_dist(i,x) + atlas.get_crow_flies_dist(i,x)
-                print("node being expanded")
-                print(i)
-                print("next possible node")
-                print(x)
+                next_item["{}".format(x)]=(atlas.get_road_dist(i,x))
+                next_item["hero"] = atlas.get_crow_flies_dist(x,num_cities-1)
+                new_dict  = {}
+                num = 0
+                for c in list_added:
+                    new_dict[c] = list_added[c]
+                    num = num + 1
+                    if num == len(list_added)-1:
+                        break
+                both_list = {**new_dict.copy(), **next_item.copy()}
+
+                nodes["{}".format(i)]["{}".format(x)] = both_list.copy()
             if  len(dict_1) >0:
                 nodes["{}".format(i)]= dict_1
-
             if x == num_cities-1:
                 lowest_node = lowest_value(nodes)
                 print(lowest_node)
+                if lowest_node == 0:
+                    print("THERE IS NO SOLUTION")
+                    return None
                 i = int(lowest_node[1])
-                print(i)
-                number_to_add = nodes[lowest_node[0]][lowest_node[1]]
+
+                list_added = nodes[lowest_node[0]][lowest_node[1]].copy()
+
+                if i == num_cities-1:
+                    final_list = list(nodes[lowest_node[0]][lowest_node[1]].keys())
+                    final_values = list(nodes[lowest_node[0]][lowest_node[1]].values())
+                    final_list.insert(0,0)
+                    final_list.remove("hero")
+                    return (final_list, sum(final_values))
+                del nodes[lowest_node[0]][lowest_node[1]]
+        if len(dict_1) == 0 and x == 0:
+            break   
 
 
-                
-
-
-    print()
-    # print(nodes)
-
-                   
-
-            
-    # print(nodes)
     # Here's a (bogus) example return value:
     return ([0,3,2,4],970)
 
 def lowest_value(dict):
     tuple_dict = {}
-   
-  
     for i in dict:
-       
-       low =  min(dict[i], key=dict[i].get)
-       tuple_dict[(i,low)] = dict[i][low]
-       
-    return min(tuple_dict, key=tuple_dict.get)
+       if len(dict[i])>0:
+           low =  minimum(dict[i])
+           tuple_dict[(i,low)] = dict[i][low]
+    
+    return minimum(tuple_dict)
+
+
+def minimum(dict):
+    num = 0
+    least = 0
+    for key in dict:
+        if num == 0:
+            least = key
+        if  sum(dict[key].values()) < sum(dict[least].values()):
+            least = key
+        num = num + 1
+
+    return least
 
 
 
@@ -81,12 +112,12 @@ def lowest_value(dict):
 if __name__ == '__main__':
 
     if len(sys.argv) not in [2,3]:
-        print("Usage: gps.py numCities|atlasFile [debugLevel].")
+       # print("Usage: gps.py numCities|atlasFile [debugLevel].")
         sys.exit(1)
 
     if len(sys.argv) > 2:
         if sys.argv[2] not in ['DEBUG','INFO','WARNING','ERROR']:
-            print('Debug level must be one of: DEBUG, INFO, WARNING, ERROR.')
+           # print('Debug level must be one of: DEBUG, INFO, WARNING, ERROR.')
             sys.exit(2)
         logging.getLogger().setLevel(sys.argv[2])
     else:
